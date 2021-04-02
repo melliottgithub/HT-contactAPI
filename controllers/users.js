3fca76d27ebcfa415151ea62c39fd514d0f606a8
Michael <kllmmc23@gmail.com>
2021-04-02 17:38:23 -0600

    add registration

--- controllers/users.js
+++ controllers/users.js
@@ -5,29 +5,29 @@ const { secret } = require('../config');
 //crud
 module.exports = {
   async post(req, res, next) {
-    const { name, email, password } = req.body;
+    const { name, email, password , passwordConfirm } = req.body;
     try {
       let user = await users.Model.findOne({ email });
 
       if (user) {
-        res.json({ message: 'User already exists' });
+        return res.status(500).json({ message: 'User already exists' });
       }
-      console.log('users post ', req.body);
+      //
+      if (password !== passwordConfirm) {
+        return res.status(500).json({ message: 'Password does not match' });
+      }
       user = await users.registerUser({ name, email, password });
-      console.log('user', user);
       const { id } = user;
       const payload = {
         user: {
           id,
         },
       };
-      console.log('payload', payload);
       jwt.sign(payload, secret.key, { expiresIn: 360000 }, (err, token) => {
         if (err) {
           console.log(err);
           throw err;
         }
-        console.log('token', token)
         return res.status(200).json({ token });
       });
 
@@ -36,12 +36,12 @@ module.exports = {
 
       if (err.name == 'MongoError' && err.code === 11000) {
         if ('email' in err.keyPattern) {
-          throw new Error('Email address is already registered');
+          return res.status(400).json({ error : 'Email address is already registered' });
         } else {
-          throw new Error('Duplicated key');
+          return res.status(400).json({ error : 'Duplicate Key' });
         }
       }
-      throw err;
+      res.status(500).json({ error : err });
     }
   },
   async get(req, res, next) {
